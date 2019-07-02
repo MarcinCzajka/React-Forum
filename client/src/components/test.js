a6bee55236f67e49cd3562959600bfd93111b61f
Marcin Czajka <czajka.marcin@protonmail.com>
2019-07-02 13:14:03 +0200

    .

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -5,7 +5,7 @@ class Test extends React.Component {
 	constructor(props) {
 		super(props);
 		
-		this.state = { posts: {1: "hello test"} };
+		this.state = { posts: "hello test" };
 	}
 	
 	componentDidMount() {
@@ -13,21 +13,60 @@ class Test extends React.Component {
 	}
 	
 	render() {
-		return <div>{this.state.posts[1]}</div>
+		return <div>{this.state.posts}</div>
 	}
 
 
 	getPosts = async () => {
-		const result = await basePath({
+		await basePath({
 			method: "get",
 			url: "/api/posts/"
 		})
 		.then(res => {
+			console.log(res.data)
 			this.setState({
-				posts: res
+				posts: res.data[1]
 			});
 		});
 	}
+	
+	populatePosts = async () => {
+		await basePath({
+			method: "post",
+			url: "/api/posts/",
+			data: {
+				
+			}
+		})
+	}
+	
+    authorId: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    responseTo: {
+        type: String
+    }
+	
+	  postReservation = async () => {
+    const reservationResponse = await basePath({
+      method: "put",
+      url: "/api/screenings/" + this.props.screeningId,
+      data: {
+        selectedSeats: this.props.selectedSeats,
+        isOccupied: true
+      },
+      withCredentials: true
+    });
+	
 	
 }
 
--- server/routes/postsRouter.js
+++ server/routes/postsRouter.js
@@ -19,14 +19,4 @@ router.post("/", async (req, res) => {
 	res.send(posts);
 });
 
-router.post("/", async (req, res) => {
-    const { error } = validateMovie(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-
-    const movie = new Movie(req.body);
-
-        await movie.save();
-        res.send(movie);
-});
-
 module.exports = router;
