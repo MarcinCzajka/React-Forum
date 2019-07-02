abcc7c83960d0c1e7af283226521a979ab4fe826
Marcin Czajka <czajka.marcin@protonmail.com>
2019-07-02 09:24:28 +0200

    You can now start client and server concurrently using concurrently

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ require('./database')();
 
 app.use('/api/posts', postsRouter);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
 });
