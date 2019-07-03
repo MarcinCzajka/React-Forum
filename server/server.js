058c23816a9b4068f00660cfc7b106a40e621abc
Marcin Czajka <czajka.marcin@protonmail.com>
2019-07-03 10:05:40 +0200

    Working on getting PORT from .env

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const postsRouter = require('./routes/postsRouter');
 
 const app = express();
-require('./database')();
+require('./config/database')();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
