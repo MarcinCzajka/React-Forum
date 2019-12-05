9c4f2bf0f4289a108008e4e6c2ebb7a622d99722
Marcin Czajka <czajka.marcin@protonmail.com>
2019-12-05 18:27:01 +0100

    First stroke at cloudinary integration

--- client/src/components/AboutMe.js
+++ client/src/components/AboutMe.js
@@ -3,14 +3,30 @@ import basePath from '../api/basePath';
 import { Card, Icon, Image } from 'semantic-ui-react';
 import moment from 'moment';
 import { UserConsumer } from '../contexts/UserContext';
+import { Helmet } from "react-helmet";
 
 class AboutMe extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        const script = document.createElement("script");
+            script.src = "https://widget.cloudinary.com/v2.0/global/all.js";
+            script.async = true;
+
+        document.body.appendChild(script);
+    }
 
     render() {
         return (
             <UserConsumer>
                 {context => (
                     <Card style={{display:this.props.display}}>
+                        <Helmet>
+                            <script defer src="https://widget.cloudinary.com/v2.0/global/all.js"></script>
+                        </Helmet>
+
                         <Image src={context.userAvatar} wrapped ui={false} />
                         <Card.Content>
                         <Card.Header>{context.userName}</Card.Header>
