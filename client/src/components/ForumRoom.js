55f0707f4e97a27dfa2ebaaa9219d50f259cfb5a
Marcin Czajka <czajka.marcin@protonmail.com>
2019-12-08 21:38:36 +0100

    ForumRoom component polish

--- client/src/components/ForumRoom.js
+++ client/src/components/ForumRoom.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import basePath from '../api/basePath';
-import { Comment, Form, Button } from "semantic-ui-react";
+import { Segment, Comment, Form, Button, Image } from "semantic-ui-react";
 import moment from "moment";
 import UserContext from '../contexts/UserContext';
 
@@ -26,27 +26,26 @@ class ForumRoom extends React.Component {
 	render() {
 		return (
 			<div className="ui large comments">
-				<Comment className="comment">
-					<Comment.Avatar 
-						className="avatar" 
-						src={this.state.image} >
-					</Comment.Avatar>
+				<Segment>
+					<Comment className="comment">
 
-					<Comment.Content>
-						<Comment.Author className="author" as='a'>{this.state.authorNick}</Comment.Author>
-						<Comment.Metadata className="metadata">
-							<span className="date">{this.state.date}</span>
-						</Comment.Metadata>
-						<Comment.Text as='p' className="text">{this.state.shortDescription}</Comment.Text>
-					
-						<Form reply style={{display:(this.context.selectedPage === 'Feed' ? 'block' : 'none')}}>
-							<Form.TextArea value={this.state.replyContent} onChange={e => this.setState(e.target.value)} />
-							<Button onClick={this.handleReplyToPost} content='Add Reply' labelPosition='left' icon='edit' primary />
-						</Form>
-						
-					</Comment.Content>
+						<Comment.Content>
+							<Segment.Inline>
+								<Image src={this.state.image} size='small' rounded />
+								<Comment.Metadata className="metadata">
+									<span className="date">{this.state.date}</span>
+								</Comment.Metadata>
+								<Comment.Text as='p' className="text">{this.state.shortDescription}</Comment.Text>
+							</Segment.Inline>
+							<Form reply style={{display:(this.context.selectedPage === 'Feed' ? 'block' : 'none')}}>
+								<Form.TextArea value={this.state.replyContent} onChange={e => this.setState(e.target.value)} />
+								<Button onClick={this.handleReplyToPost} content='Add Reply' labelPosition='left' icon='edit' primary />
+							</Form>
+							
+						</Comment.Content>
 
-				</Comment>
+					</Comment>
+				</Segment>
 			</div>
 		)
 	}
