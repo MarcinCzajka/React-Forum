5cf1cd259bf8da72e4cfb9c232326c733ef8798f
Marcin Czajka <czajka.marcin@protonmail.com>
2019-12-19 10:13:33 +0100

    Deploy on heroku with minimal differences. Change root server file to index.js

--- Nowy folder/index.js
+++ Nowy folder/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const loginRouter = require('./routes/loginRouter');
 const userRouter = require('./routes/userRouter');
@@ -21,6 +22,14 @@ app.use('/login', loginRouter);
 app.use('/api/users', userRouter);
 app.use('/api/rooms', forumRoomRouter);
 app.use('/api/posts', postsRouter);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../react-ui/build')));
+}
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const loginRouter = require('./routes/loginRouter');
+const userRouter = require('./routes/userRouter');
+const forumRoomRouter = require('./routes/forumRoomRouter');
+const postsRouter = require('./routes/postsRouter');
+
+const app = express();
+require('./config/database')();
+
+app.enable('strict routing')
+
+app.use(helmet());
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/login', loginRouter);
+app.use('/api/users', userRouter);
+app.use('/api/rooms', forumRoomRouter);
+app.use('/api/posts', postsRouter);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../react-ui/build')));
+}
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`)
+});
