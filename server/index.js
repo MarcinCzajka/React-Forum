73228b3c6fdcb6f0514db98aea58fadbe2b11152
Marcin Czajka <czajka.marcin@protonmail.com>
2019-12-30 16:13:43 +0100

    Limit cloudinary API to only allow 1 file at a time and max 5mb

--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ app.use('/api/users', userRouter);
 app.use('/api/rooms', forumRoomRouter);
 app.use('/api/posts', postsRouter);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-}
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
