785c80ecb94601657d58adecb2e7e0104d7f65db
Marcin Czajka <czajka.marcin@protonmail.com>
2020-02-18 09:32:12 +0100

    .

--- client/src/components/post/comments/comment/PostComment.jsx
+++ client/src/components/post/comments/comment/PostComment.jsx
@@ -5,7 +5,7 @@ import CommentGroup from '../CommentGroup';
 import CommentToolkit from '../replyForm/CommentToolkit';
 import PostPlaceholder from '../../../placeholders/PostPlaceholder';
 import AvatarPlaceholder from '../../../placeholders/AvatarPlaceholder';
-import { LocaleConsumer } from '../../contexts/LocaleContext';
+import { LocaleConsumer } from '../../../../contexts/LocaleContext';
 import './PostComment.css';
 
 class PostComment extends React.Component {
@@ -13,7 +13,10 @@ class PostComment extends React.Component {
 		super(props);
 		
 		this.state = {
-			avatarReady: false
+			avatarReady: false,
+			date: moment(this.props.date)
+				.format("MMMM Do YYYY, h:mm:ss")
+				.toString()
 		}
 
 		this.handleReplyRef = React.createRef();
@@ -28,60 +31,57 @@ class PostComment extends React.Component {
 	onAvatarLoad = () => {
 		this.setState({avatarReady: true});
 	}
-
-	formattedDate = (locale) => {
-		return moment(this.props.date)
-			.format("MMMM Do YYYY, h:mm:ss")
-			.toString()
-			.locale(locale);
-	}
 	
 	render() {
 		return (
-			<div className="ui large comments">
+			<LocaleConsumer>
+				{locale => (
+					<div className="ui large comments">
 
-				{this.props.showPlaceholder ? (
-					<PostPlaceholder />
-				) : (
-					<Comment className="comment">
+						{this.props.showPlaceholder ? (
+							<PostPlaceholder />
+						) : (
+							<Comment className="comment">
 
-						{!this.state.avatarReady ? (
-							<AvatarPlaceholder size='sizePostComment' /> 
-						) : ''}
+								{!this.state.avatarReady ? (
+									<AvatarPlaceholder size='sizePostComment' /> 
+								) : ''}
 
-						<Comment.Avatar 
-							style={{display:(this.state.avatarReady ? 'block' : 'none')}}
-							className="avatar" 
-							src={this.props.avatar}
-							onLoad={this.onAvatarLoad}>
-						</Comment.Avatar>
-						
-						<Comment.Content>
-							<Comment.Author className="author" as='a'>{this.props.authorNick}</Comment.Author>
-							<Comment.Metadata className="metadata">
-								<span className="date">{this.formattedDate()}</span>
-							</Comment.Metadata>
-							<Comment.Text as='p' className="text">{this.props.content}</Comment.Text>
+								<Comment.Avatar 
+									style={{display:(this.state.avatarReady ? 'block' : 'none')}}
+									className="avatar" 
+									src={this.props.avatar}
+									onLoad={this.onAvatarLoad}>
+								</Comment.Avatar>
+								
+								<Comment.Content>
+									<Comment.Author className="author" as='a'>{this.props.authorNick}</Comment.Author>
+									<Comment.Metadata className="metadata">
+										<span className="date">{this.state.date}</span>
+									</Comment.Metadata>
+									<Comment.Text as='p' className="text">{this.props.content}</Comment.Text>
 
-						<CommentToolkit 
-							postId={this.props.postId}
-							roomId={this.props.roomId}
-							authorId={this.props.authorId}
-							userId={this.props.userId}
-							handleReply={this.handleReplyToPost}
-							removeComment={this.props.removeComment}
-						/>
-							
-						</Comment.Content>
-					</Comment>
+								<CommentToolkit 
+									postId={this.props.postId}
+									roomId={this.props.roomId}
+									authorId={this.props.authorId}
+									userId={this.props.userId}
+									handleReply={this.handleReplyToPost}
+									removeComment={this.props.removeComment}
+								/>
+									
+								</Comment.Content>
+							</Comment>
+						)}
+
+						<CommentGroup 
+							parentId={this.props.postId} 
+							ref={this.handleReplyRef} >
+						</CommentGroup>
+
+					</div>
 				)}
-
-				<CommentGroup 
-					parentId={this.props.postId} 
-					ref={this.handleReplyRef} >
-				</CommentGroup>
-
-			</div>
+			</LocaleConsumer>
 		);
 	}
 
